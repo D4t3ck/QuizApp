4a478945a70f9006e8ef390f743a5586cd2f0028
Daniel Sinteck <d.sint3ck@gmail.com>
2024-01-10 12:31:15 +0100

    Added Progress Bar

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ let questions = [
   {
     question: "Wie viele Planeten hat unser Sonnensystem?",
     answer_1: "7",
-    answer_2: "8",
-    answer_3: "9",
+    answer_2: "9",
+    answer_3: "5",
     answer_4: "10",
     right_answer: 2,
   },
@@ -55,10 +55,35 @@ let questions = [
     answer_4: "Michelangelo",
     right_answer: 3,
   },
+  {
+    question: "Welches ist das größte Säugetier der Welt?",
+    answer_1: "Elefant",
+    answer_2: "Giraffe",
+    answer_3: "Blauwal",
+    answer_4: "Nashorn",
+    right_answer: 3,
+  },
+  {
+    question: "Wer schrieb 'Romeo und Julia'?",
+    answer_1: "Charles Dickens",
+    answer_2: "William Shakespeare",
+    answer_3: "Jane Austen",
+    answer_4: "Emily Brontë",
+    right_answer: 2,
+  },
+  {
+    question: "Welches Land ist das flächenmäßig größte der Welt?",
+    answer_1: "China",
+    answer_2: "Russland",
+    answer_3: "USA",
+    answer_4: "Indien",
+    right_answer: 2,
+  },
 ];
 
+let rightQuestions = 0;
 let currentQuestion = 0;
-let currentAnswer = 0;
+
 
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length;
@@ -67,13 +92,31 @@ function init() {
 }
 
 function showQuestion() {
-  let question = questions[currentQuestion];
+  if (currentQuestion >= questions.length) {
+    // Show end screen
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
 
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+    document.getElementById('header-image').src = '' ;
+  } else { // Show question
+
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress-bar').innerHTML = `${percent} %`;
+    document.getElementById('progress-bar').style = `width: ${percent}%;`;
+    console.log('Fortschritt:', percent, '%');
+
+    let question = questions[currentQuestion];
+
+    document.getElementById("question-number").innerHTML = currentQuestion + 1;
+    document.getElementById("questiontext").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
 }
 
 function answer(selection) {
@@ -81,31 +124,32 @@ function answer(selection) {
   let selectedQuestionNumber = selection.slice(-1);
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
 
-  if (selectedQuestionNumber == question["right_answer"]) {
-    console.log("Richtige Antwort!!");
+  if (selectedQuestionNumber == question["right_answer"]) { // Rightige Frage beantwortet
     document.getElementById(selection).parentNode.classList.add("bg-success");
+    rightQuestions++; // Zähler für richtige Fragen
   } else {
     document.getElementById(selection).parentNode.classList.add("bg-danger");
-    document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+    document
+      .getElementById(idOfRightAnswer)
+      .parentNode.classList.add("bg-success");
   }
-  document.getElementById('next-button').disabled = false;
+  document.getElementById("next-button").disabled = false;
 }
 
 function nextQuestion() {
   currentQuestion++; // z.B. von 0 auf 1
-  document.getElementById('next-button').disabled = true;
-  resetAnswerButtons()
+  document.getElementById("next-button").disabled = true;
+  resetAnswerButtons();
   showQuestion();
 }
 
 function resetAnswerButtons() {
-  document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
-  document.getElementById('answer_1').parentNode.classList.remove('bg-success');
-  document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
-  document.getElementById('answer_2').parentNode.classList.remove('bg-success');
-  document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
-  document.getElementById('answer_3').parentNode.classList.remove('bg-success');
-  document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
-  document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+  document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_1").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-success");
 }
-
